cfd03149d1d952443d20935bfb76837117a177e2
Faysal Rahman <64473571+ftowaha@users.noreply.github.com>
2021-04-23 16:11:34 +0600

    Create Ekka Dokka.cpp

---
+++ 1116/Ekka Dokka.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int T,x,i,j,m;
+    long int w;
+    scanf("%d",&T);
+    for(x =0; x<T; x++) 
+        {
+            scanf("%ld",&w);
+            if(w%2!=0)
+                printf("Case %d: Impossible\n",x+1);
+            else 
+            {
+                m = 1;
+                while(w%2==0) 
+                    {
+                        m*=2;
+                        w/=2;
+
+                    }
+                printf("Case %d: %ld %d\n",x+1,w,m);
+            }
+
+        }
+    return 0;
+}
